0c6f277e84b77f52349e00380d0c66eb3c9ac0e1
edopandoyo <edopandoyo@gmail.com>
2020-05-20 05:16:07 +0700

    Updates

---
+++ precache-manifest.02c06bba802cff82e3254e5694e8045d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6584700a7dfae2b6b6e2ad8bb81f9921",
+    "url": "/warungmu/index.html"
+  },
+  {
+    "revision": "95f43b3daf5d83b0e9ee",
+    "url": "/warungmu/static/css/2.8919110f.chunk.css"
+  },
+  {
+    "revision": "5127b03755c66176dc3e",
+    "url": "/warungmu/static/css/main.54ea6357.chunk.css"
+  },
+  {
+    "revision": "95f43b3daf5d83b0e9ee",
+    "url": "/warungmu/static/js/2.c0f45b57.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/warungmu/static/js/2.c0f45b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5127b03755c66176dc3e",
+    "url": "/warungmu/static/js/main.10e630f8.chunk.js"
+  },
+  {
+    "revision": "dcbba3689391da54ce9b",
+    "url": "/warungmu/static/js/runtime-main.8607b527.js"
+  },
+  {
+    "revision": "15b4ad591e9c25d9271a8073992f0e58",
+    "url": "/warungmu/static/media/logo.15b4ad59.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/warungmu/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/warungmu/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/warungmu/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/warungmu/static/media/slick.f97e3bbf.svg"
+  }
+]);
